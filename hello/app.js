732658131b4525e438ad283d97b101b1e1defdb4
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-23 10:18:06 +0100

    la requete n'est pas pris en compte, la reponse si tout se passe bien (statut 200) est Hello World

--- hello/app.js
+++ hello/app.js
@@ -3,10 +3,8 @@ const http = require("http");
 const hostname = "127.0.0.1";
 const port = 3000
 
-const server = http.createServer(  (requ, res) => {
+const server = http.createServer(  (req, res) => {
     res.statusCode = 200
-    res.setHeader()
-
-}
-
-)
+    res.setHeader("Content-Type", "text/plain")
+    res.end("Hello World!")
+})
