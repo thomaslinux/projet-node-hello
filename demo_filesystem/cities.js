45e4eb49777659a333dbb5d354d56dc9365472ad
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-23 15:36:58 +0100

    formating

--- demo_filesystem/cities.js
+++ demo_filesystem/cities.js
@@ -3,9 +3,9 @@ const fs = require("fs")
 function readCities() {
     let cities = ""
     try {
-        cities = fs.readFileSync("cities.csv","utf8")
+        cities = fs.readFileSync("cities.csv", "utf8")
     } catch (err) {
-        if(err.code === 'ENOENT') {
+        if (err.code === 'ENOENT') {
             console.error("Le fichier n'existe pas.")
         } else {
             console.error(err);
@@ -14,14 +14,14 @@ function readCities() {
     return cities.split("\n");
 }
 
-let cities = [ 'Nantes', 'Rennes', 'Quimper', 'Paris' ];
+let cities = ['Nantes', 'Rennes', 'Quimper', 'Paris'];
 // fs.rm("cities.csv")
 fs.writeFileSync("cities.csv", cities.join("\n"))
 
 cities = readCities();
 cities.forEach(city => console.log("ville : ", city));
 
-fs.appendFileSync("cities.csv","\nLorient","utf8")
+fs.appendFileSync("cities.csv", "\nLorient", "utf8")
 
 cities = readCities();
 cities.forEach(city => console.log("ville : ", city));
