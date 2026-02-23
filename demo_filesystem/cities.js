08b91f4c95677d688c104f4e51e9f146d3d0cb39
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-23 11:44:56 +0100

    recupère les cities ou renvoie une erreur

--- demo_filesystem/cities.js
+++ demo_filesystem/cities.js
@@ -3,6 +3,12 @@ const fs = require("fs")
 function readCities() {
     let cities = ""
     try {
-
+        cities = fs.readFileSync("cities.csv","utf8")
+    } catch (err) {
+        if(err.code === 'ENOENT') {
+            console.error("Le fichier n'existe pas.")
+        } else {
+            console.error(err);
+        }
     }
 }
