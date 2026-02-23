5fc943cac906803499fb2701a3f8606a4aa2bde6
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-23 13:49:37 +0100

    création d'une calculatrice

---
+++ demo_package/calculatrice.js
@@ -0,0 +1,18 @@
+function addition(a, b) {
+    return a + b;
+}
+
+function soustraction(a, b) {
+    return a - b;
+}
+
+function multiplication(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    if (b === 0) {
+        throw new Error("La division par 0 est interdite");
+    }
+    return a / b;
+}
