813ceb525a6f4e2871753dbe6188a5f8a1398351
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-23 16:23:43 +0100

    récupérer l'id donné dans l'url

--- API express v2/app.js
+++ API express v2/app.js
@@ -13,11 +13,22 @@ app.use((req, res, next) => {
     " user-agent: ",
     req.get("User-Agent"),
   );
+  next();
 });
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+const cities = "Nantes, Rennes, Lorient, Bordeaux, Quimper".split(", ");
+
+app.get("/cities", (req, res) => {
+    res.send(cities.join(", "))
+})
+
+app.get("/cities/:id", (req, res) => {
+    res.send(cities[req.params.id])
+})
 
 app.listen(port, () => {
   console.log("l'api ecoute sur le port : ", port);
