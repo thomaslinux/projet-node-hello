3db9d07df1b02022b6a69256afb896bede6ec328
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-24 14:35:29 +0100

    find the random number made in express.js

---
+++ TP express random number/random-number/routes/index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var router = express.Router();
+
+const numberToFind = "99";
+const lastNumberInput = "0";
+const MAX = 100;
+const MIN = 0;
+const randomNumber = Math.floor(Math.random() * (MAX - MIN) + MIN);
+const numberFound = false;
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Express Brains",
+    subtitle:
+      "Découvrez en un minimum de tour le nombre qui se cache derrière la carte mystère",
+    lastNumberInput: lastNumberInput,
+  });
+});
+
+module.exports = router;
