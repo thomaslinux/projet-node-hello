9cd0d24c95f8c22294c3c451bd77af607335af3b
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-24 09:41:03 +0100

    urlencode

--- API express v2/app.js
+++ API express v2/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:true}));
 
 app.use((req, res, next) => {
   console.log(
