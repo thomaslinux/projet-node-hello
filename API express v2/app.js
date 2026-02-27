efcd0364b528caa47784e31848acc3cddfe2c371
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2026-02-27 14:00:19 +0100

    regex pour récupérer les autorisations dans le header du token

--- API express v2/app.js
+++ API express v2/app.js
@@ -249,6 +249,7 @@ function extractBearerToken(headerValue) {
   if (typeof headerValue !== "string") {
     return false;
   }
+  const matches = headerValue.match(/(bearer)\s+(\S+)/i);
 }
 
 app.get("/cities", (req, res) => {
